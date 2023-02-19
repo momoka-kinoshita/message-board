9e2374574c3945556f95ec80c59c278aa80afaca
momoka.kinoshita <momokakinoshita0302@gmail.com>
2023-02-19 09:07:04 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,16 @@
 class MessagesController < ApplicationController
-  
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
   def index
     @messages = Message.all
   end
 
-
   def show
-    @message = Message.find(params[:id])
   end
-
 
   def new
     @message = Message.new
   end
-
 
   def create
     @message = Message.new(message_params)
@@ -27,15 +24,10 @@ class MessagesController < ApplicationController
     end
   end
 
-
   def edit
-    @message = Message.find(params[:id])
   end
 
-
   def update
-    @message = Message.find(params[:id])
-
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
@@ -45,21 +37,20 @@ class MessagesController < ApplicationController
     end
   end
 
-
   def destroy
-    @message = Message.find(params[:id])
     @message.destroy
 
     flash[:success] = 'Message は正常に削除されました'
     redirect_to messages_url
   end
-  
-  
+
   private
 
-  # Strong Parameter
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
   def message_params
     params.require(:message).permit(:content)
   end
-  
 end
